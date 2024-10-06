7fce03044b12ded3eaed693dc40213b5507d0cef
Mel <angel.melendez.c@gmail.com>
2024-10-06 15:37:07 -0400

    set up siwe + username registration thru dynamic

--- src/app/cartera/page.tsx
+++ src/app/cartera/page.tsx
@@ -4,12 +4,14 @@ import Link from 'next/link'
 
 import PageWithAppbar from '@/components/layout/pageWithAppbar'
 import { Button } from '@/components/ui/button'
+import { useDynamicContext } from '@dynamic-labs/sdk-react-core'
 
 export default function Cartera() {
+  const { user } = useDynamicContext()
   return (
     <PageWithAppbar>
       <div className="page gap-y-8 text-center">
-        <h2>Hola</h2>
+        <h2>Hola {user?.username}</h2>
         <Link href="/">
           <Button className={`mt-6 h-12 text-lg md:mt-8 lg:mt-8 xl:mt-12`}>
             Atrás
