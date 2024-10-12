19c94bea6e5c6242c31af2823b8b9145be2b49d2
mel <angel.melendez.c@gmail.com>
2024-10-12 17:16:38 -0600

    add baseSepolia, minor copy changes, update navbar

---
+++ src/app/ahorro/page.tsx
@@ -0,0 +1,18 @@
+import PageWithAppbar from '@/components/layout/pageWithAppbar'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+export default function Beneficios() {
+  return (
+    <PageWithAppbar>
+      <div className="page gap-y-8 text-center">
+        <h2>Cuenta Sustantiva</h2>
+        <Link href="/">
+          <Button className={`mt-6 h-12 text-lg md:mt-8 lg:mt-8 xl:mt-12`}>
+            Atrás
+          </Button>
+        </Link>
+      </div>
+    </PageWithAppbar>
+  )
+}
